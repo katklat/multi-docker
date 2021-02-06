6938ce418d20ded70a041489220bb42d9a890660
katklat <kinz@protonmail.com>
2021-02-06 08:55:50 +0100

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders without crashing', () => {
+  //render(<App />);
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+});
+
+// test will not work without express server running
